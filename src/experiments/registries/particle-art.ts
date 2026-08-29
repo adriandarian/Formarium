916ffec6d96e221c1320a3e247ee63d61c29f227
Adrian Darian <adrian.the.hactus@gmail.com>
2026-08-28 18:48:13 -0700

    refactor: isolate particle art renderers

---
+++ src/experiments/registries/particle-art.ts
@@ -0,0 +1,4 @@
+import type { ExperimentLoader } from '../types'
+
+export const particleStageLoaders = {} satisfies Record<string, ExperimentLoader>
+export const particlePreviewLoaders = {} satisfies Record<string, ExperimentLoader>
